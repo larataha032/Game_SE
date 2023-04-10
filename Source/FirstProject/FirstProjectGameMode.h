5027fda51bedcf2263fb41ed66e4ebf824ecb65a
larataha032 <90355775+larataha032@users.noreply.github.com>
2023-04-10 09:04:15 -0400

    SE Game Project
    10 April 2023

---
+++ Source/FirstProject/FirstProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirstProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFirstProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFirstProjectGameMode();
+};
+
+
+
